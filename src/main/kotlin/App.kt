fbf2b8da03de29f2c3cdf77591938cbb5aed45e5
andrewcmc <tan.cmc@pomelofashion.com>
2021-12-29 15:47:06 +0700

    feat(npm): using packages from npm

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -11,6 +11,18 @@ import styled.styledDiv
 
 val app = fc<Props> {
     var currentVideo: Video? by useState(null)
+    var unwatchedVideos: List<Video> by useState(
+        listOf(
+            Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+            Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+            Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+        )
+    )
+    var watchedVideos: List<Video> by useState(
+        listOf(
+            Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+        )
+    )
 
     h1 {
         +"KotlinConf Explorer"
@@ -42,20 +54,21 @@ val app = fc<Props> {
             }
         }
     }
-    styledDiv {
-        css {
-            position = Position.absolute
-            top = 10.px
-            right = 10.px
-        }
-        h3 {
-            +"John Doe: Building and breaking things"
-        }
-        img {
+    currentVideo?.let { curr ->
+        child(videoPlayer) {
             attrs {
-                src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                video = curr
+                unwatchedVideo = curr in unwatchedVideos
+                onWatchedButtonPressed = {
+                    if (video in unwatchedVideos) {
+                        unwatchedVideos = unwatchedVideos - video
+                        watchedVideos = watchedVideos + video
+                    } else {
+                        watchedVideos = watchedVideos - video
+                        unwatchedVideos = unwatchedVideos + video
+                    }
+                }
             }
         }
     }
-
 }
---
+++ src/main/kotlin/ReactYoutube.kt
@@ -0,0 +1,11 @@
+@file:JsModule("react-youtube-lite")
+@file:JsNonModule
+
+import react.*
+
+@JsName("ReactYouTubeLite")
+external val reactPlayer: ComponentClass<ReactYouTubeProps>
+
+external interface ReactYouTubeProps : Props {
+    var url: String
+}
